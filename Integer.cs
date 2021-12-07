76f3eba9edee28de2f9e58645453e6c137a63ccf
pashwiniis <pashwini684@gmail.com>
2021-12-07 15:09:48 +0530

    Find the maximum float number

--- Integer.cs
+++ Integer.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace Generics
 {
-     class Integer
+     class FindMaxValue 
     {
 
 
-        public static int FindMaxThree(int first, int second, int third)
+        public static int FindMaxInteger(int first, int second, int third)
         {
             int result = first.CompareTo(second);
             int res = first.CompareTo(third);
@@ -36,6 +36,33 @@ namespace Generics
                 return default;
             }
         }
-        
+
+        public static  float  FindMaxInteger(float first, float second, float third)
+        {
+            float result = first.CompareTo(second);
+            float res = first.CompareTo(third);
+
+            if ((first.CompareTo(second)>0) && (first.CompareTo(third)>0))
+            {
+                return first;
+            }
+
+            else if ((second.CompareTo(first)>0) && (second.CompareTo(third)>0))
+            {
+                return second;
+            }
+
+            else if ((third.CompareTo(first)>0) && (third.CompareTo(second)>0))
+            {
+                return third;
+            }
+
+            else
+            {
+                Console.WriteLine("All three numbers having same Value");
+                return default;
+            }
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -13,8 +13,10 @@ namespace Generics
         {
             Console.WriteLine("************Welcome to GenericsConcept**************");
 
-            
-            Console.WriteLine("The maximum Integer Value is " +Integer.FindMaxThree(30, 46, 23));
+                      
+            Console.WriteLine("The maximum Integer Value is " + FindMaxValue.FindMaxInteger(30, 46, 23));
+            Console.WriteLine("The maximum Float Value is " + FindMaxValue.FindMaxInteger(3.6f,6.7f,2.3f));
+
 
         }
     }
